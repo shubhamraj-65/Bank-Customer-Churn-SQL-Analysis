314491c3094ddb0ae69586989b8097d4fa06b825
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-22 16:50:26 +0530

    Add geography-wise churn rate calculation
    Added query to calculate geography-wise churn rate.

--- sql/06_churn_analysis.sql
+++ sql/06_churn_analysis.sql
@@ -15,3 +15,7 @@ FROM churn_bank;
 select geography,count(*) as geo_churned from churn_bank
 where Exited=1
 group by Geography;
+
+-- 5. Geography-wise churn rate nikalo.
+select geography,avg(exited) *100 as churn_rate from churn_bank
+group by Geography;
