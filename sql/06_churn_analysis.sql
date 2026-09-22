56e129597917f0b597f4b17963b660711ed2d2fe
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-22 16:50:05 +0530

    adding query in section 6

--- sql/06_churn_analysis.sql
+++ sql/06_churn_analysis.sql
@@ -10,3 +10,8 @@ where Exited=0;
 SELECT
     COUNT(CASE WHEN Exited = 1 THEN 1 END) * 100.0 / COUNT(*) AS overall_churn_rate
 FROM churn_bank;
+
+-- 4. Geography-wise churned customers nikalo.
+select geography,count(*) as geo_churned from churn_bank
+where Exited=1
+group by Geography;
