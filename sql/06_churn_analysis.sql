abae0fe8f592aa824a09c4231ba0db2aae44465a
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-23 10:47:39 +0530

    Update 06_churn_analysis.sql

--- sql/06_churn_analysis.sql
+++ sql/06_churn_analysis.sql
@@ -1,6 +1,3 @@
--- 1. Total churned customers kitne hain?
-select count(*) as churned_customer from churn_bank
-where Exited=1;
 
 -- 2. Total non-churned customers kitne hain?
 select count(*) as non_churned from churn_bank
