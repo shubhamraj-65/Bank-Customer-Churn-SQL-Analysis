0df33db7a7cb7bc5f29b731f301e6bd0946ea4b4
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-23 10:50:24 +0530

    Update churn analysis SQL query
    Removed comparison of churn rates for active and inactive members.

--- sql/06_churn_analysis.sql
+++ sql/06_churn_analysis.sql
@@ -3,12 +3,7 @@ select geography,avg(exited) *100 as churn_rate from churn_bank
 group by Geography;
 
 
--- 7. Active vs inactive members ka churn rate compare karo.
-select avg(exited) * 100 as churn_rate from churn_bank
-where IsActiveMember =1;
 
-select avg(exited) * 100 as churn_rate from churn_bank
-where IsActiveMember =0;
 
 
 
