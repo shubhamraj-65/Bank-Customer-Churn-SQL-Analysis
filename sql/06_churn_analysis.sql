1f2c01fca1e9dc4e90f13af31e96267c1b2da5a2
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-23 10:48:31 +0530

    Delete overall churn rate calculation from SQL
    Removed overall churn rate calculation query.

--- sql/06_churn_analysis.sql
+++ sql/06_churn_analysis.sql
@@ -1,9 +1,3 @@
-
--- 3. Overall churn rate calculate karo.
-SELECT
-    COUNT(CASE WHEN Exited = 1 THEN 1 END) * 100.0 / COUNT(*) AS overall_churn_rate
-FROM churn_bank;
-
 -- 4. Geography-wise churned customers nikalo.
 select geography,count(*) as geo_churned from churn_bank
 where Exited=1
