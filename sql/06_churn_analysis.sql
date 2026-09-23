aef782dd04e23a2a2fc5eb965dadbff4a5eda469
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-23 10:47:53 +0530

    Delete non-churned customers query
    Removed query for total non-churned customers.

--- sql/06_churn_analysis.sql
+++ sql/06_churn_analysis.sql
@@ -1,8 +1,4 @@
 
--- 2. Total non-churned customers kitne hain?
-select count(*) as non_churned from churn_bank
-where Exited=0;
-
 -- 3. Overall churn rate calculate karo.
 SELECT
     COUNT(CASE WHEN Exited = 1 THEN 1 END) * 100.0 / COUNT(*) AS overall_churn_rate
