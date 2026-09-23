a6349239c8a318d3446ee1daed31ceae001b3bdd
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-23 10:49:20 +0530

    Remove age-wise churn rate calculation
    Removed age-wise churn rate calculation from the SQL script.

--- sql/06_churn_analysis.sql
+++ sql/06_churn_analysis.sql
@@ -27,9 +27,6 @@ group by NumOfProducts;
 select tenure,avg(exited) * 100 as churn_rate from churn_bank
 group by Tenure;
 
--- 11. Age-wise churn rate nikalo.
-select age,avg(exited) * 100 as churn_rate 
-from churn_bank 
-group by age;
+
 
 
