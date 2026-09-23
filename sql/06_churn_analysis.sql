e727fdf4301fb9f14f490ca7194876047ec12790
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-23 10:50:11 +0530

    Remove credit-card holder churn rate comparison
    Removed comparison of churn rates between credit-card holders and non-holders.

--- sql/06_churn_analysis.sql
+++ sql/06_churn_analysis.sql
@@ -10,13 +10,7 @@ where IsActiveMember =1;
 select avg(exited) * 100 as churn_rate from churn_bank
 where IsActiveMember =0;
 
--- 8. Credit-card holders vs non-holders ka churn rate compare karo.
-select avg(exited) * 100 as churn_rate from 
-churn_bank
-where HasCrCard=1;
-select avg(exited) * 100 as churn_rate from 
-churn_bank
-where HasCrCard=0;
+
 
 
 
