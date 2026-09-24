d96f01d4cd1243396b5b50fcfc332e3e47a103e6
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-24 11:15:17 +0530

    Add query for customers with below average credit score

--- sql/07_subqueries_and_ctes.sql
+++ sql/07_subqueries_and_ctes.sql
@@ -7,3 +7,6 @@ SELECT * FROM churn_bank WHERE Balance > ( SELECT AVG(Balance) FROM churn_bank )
 
 -- 2. Find customers whose salary is above the overall average salary. 
 SELECT * FROM churn_bank WHERE EstimatedSalary > ( SELECT AVG(EstimatedSalary) FROM churn_bank );
+
+-- 3. Find customers whose credit score is below the overall average credit score.
+SELECT * FROM churn_bank WHERE CreditScore < ( SELECT AVG(CreditScore) FROM churn_bank );
