9753a579ca1a7747795de6c56691c866c9fded2d
shubhamraj-65 <shubhamraj.1937@gmail.com>
2026-09-24 11:14:54 +0530

    Add query to find customers with above-average salary

--- sql/07_subqueries_and_ctes.sql
+++ sql/07_subqueries_and_ctes.sql
@@ -4,3 +4,6 @@
 
 -- 1. Find customers whose balance is above the overall average balance. 
 SELECT * FROM churn_bank WHERE Balance > ( SELECT AVG(Balance) FROM churn_bank );
+
+-- 2. Find customers whose salary is above the overall average salary. 
+SELECT * FROM churn_bank WHERE EstimatedSalary > ( SELECT AVG(EstimatedSalary) FROM churn_bank );
